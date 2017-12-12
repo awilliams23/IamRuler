78e2676e6a48af2bebe3d9747171c07c9037a909
awilliams23 <awxtd@mail.umkc.edu>
2017-12-11 18:59:59 -0600

    Iteration 3

---
+++ app/src/main/java/com/iamruler/iamruler/SettingsActivity.java
@@ -0,0 +1,27 @@
+package com.iamruler.iamruler;
+
+import android.preference.PreferenceActivity;
+import android.os.Bundle;
+
+import java.util.List;
+
+public class SettingsActivity extends PreferenceActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onBuildHeaders(List<Header> target)
+    {
+        loadHeadersFromResource(R.xml.headers_preference, target);
+        setContentView(R.layout.activity_settings);
+    }
+
+    @Override
+    protected boolean isValidFragment(String fragmentName)
+    {
+        return MyPreferenceFragment.class.getName().equals(fragmentName);
+    }
+}
