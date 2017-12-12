aaa958e2f23235c4ca7f51ab3a0dcd8ccf667aaf
awilliams23 <awxtd@mail.umkc.edu>
2017-12-11 18:44:31 -0600

    Iteration 4

---
+++ app/src/main/java/com/iamruler/iamruler/AssertSettings.java
@@ -0,0 +1,7 @@
+package com.iamruler.iamruler;
+
+public class AssertSettings {
+    public static final boolean PRIORITY1_ASSERTIONS = true;
+    public static final boolean PRIORITY2_ASSERTIONS = false;
+    public static final boolean PRIORITY3_ASSERTIONS = false;
+}
